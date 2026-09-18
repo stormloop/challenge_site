d566df672d4df0f4bde25e806b3694074ef1071d
STORM VANDEWIJER <storm.vandewijer@gmail.com>
2026-09-18 14:34:08 +0200

    log auth token error

--- backend/auth_manager.py
+++ backend/auth_manager.py
@@ -94,6 +94,7 @@ def get_current_claims(
             )
         return claims
     except jwt.PyJWTError:
+        print(f"JWT validation failed: {type(exc).__name__}: {exc}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid access token",
